126ade4c4f400c36f3240bbfed083ac6f81e1eca
Dominic Shore <dominicshore@gmail.com>
2019-04-24 17:30:53 +1000

    Upload from home computer Almost there on gender impact of tft removal  - need to remove net concept from gender metrics in next session (comments in-line)

--- 190424-Tax_Free_Thresholds.R
+++ 190424-Tax_Free_Thresholds.R
@@ -3,9 +3,9 @@ library("readxl")
 library("janitor")
 
 # Local path to Excel file containing 2017 Tax Stats individuals table 2B
-file_name <- "C:/Users/Dominic/Downloads/ts17individual02lodgmentmethodsextaxablestatusstateageyear.xlsx"
+file_name <- "./ts17individual02lodgmentmethodsextaxablestatusstateageyear.xlsx"
 
-# Importing annual (2010-11 to 2016-17) file with janitor name repair 
+# Importing annual (2010-11 to 2016-17) file with janitor name repair
 indiv_tax_stats <- read_excel(path = file_name, sheet = "Individuals Table 2B", skip = 2, .name_repair = make_clean_names)
 
 
@@ -22,7 +22,7 @@ personal_income_tax <- tibble(
 )
 
 # Calculating __average taxable income__ and __total income or loss__ per cohort
-indiv_tax_stats <- indiv_tax_stats %>% 
+indiv_tax_stats <- indiv_tax_stats %>%
   mutate(
     avg_sal_and_wage = salary_or_wages / salary_or_wages_no,
     avg_tot_y_or_l   = total_income_or_loss3 / total_income_or_loss3_no,
@@ -32,22 +32,18 @@ indiv_tax_stats <- indiv_tax_stats %>%
       avg_tot_y_or_l <= 90000 ~ ((avg_tot_y_or_l - 37001) * 0.3250) + 3572,
       avg_tot_y_or_l <= 18000 ~ ((avg_tot_y_or_l - 90001) * 0.3700) + 20797,
       TRUE ~ ((avg_tot_y_or_l - 180001) * 0.4500) + 54097
-    )) %>% 
+    )) %>%
   select(1:9, avg_sal_and_wage, everything())
 
-# Graph of incomes for taxpayers with "Non Taxable" designation 
-indiv_tax_stats %>% 
-  filter(taxable_status == "Non Taxable") %>% 
-  ggplot(aes(x = avg_sal_and_wage)) + 
+# Graph of incomes for taxpayers with "Non Taxable" designation
+indiv_tax_stats %>%
+  filter(taxable_status == "Non Taxable") %>%
+  ggplot(aes(x = avg_sal_and_wage)) +
   geom_histogram()
 
 # It turns out that the Baby Boomers are the ones breaking the $20,000 threshold
 # of "Non Taxable"
-indiv_tax_stats %>% 
-  filter(taxable_status == "Non Taxable", avg_sal_and_wage >20000) %>% 
+indiv_tax_stats %>%
+  filter(taxable_status == "Non Taxable", avg_sal_and_wage >20000) %>%
   distinct(age_range2)
 
-
-
-df <- left_join(indiv_tax_stats, personal_income_tax, by = c("avg_tot_y_or_l" = "income")) %>% 
-  select(1:9, avg_sal_and_wage, avg_tot_y_or_l, pit)
